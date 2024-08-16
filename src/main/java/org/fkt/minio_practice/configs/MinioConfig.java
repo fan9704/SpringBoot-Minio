ed32444cc08beb0f077a6d4fa21daef558b6ff9c
fan9704 <cxz123499@gmail.com>
2024-08-16 14:11:39 +0800

    feat: Cors/Swagger/Minio Config

---
+++ src/main/java/org/fkt/minio_practice/configs/MinioConfig.java
@@ -0,0 +1,26 @@
+package org.fkt.minio_practice.configs;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+    @Value("${spring.minio.url}")
+    private String url;
+
+    @Value("${spring.minio.access-key}")
+    private String accessKey;
+
+    @Value("${spring.minio.secret-key}")
+    private String accessSecret;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, accessSecret)
+                .build();
+    }
+}
